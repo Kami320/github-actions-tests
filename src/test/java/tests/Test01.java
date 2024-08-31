b2d917b9eed78f151d877659152bc3ca8cd95dbe
Kami320 <kamiryaaa@gmail.com>
2024-08-31 16:18:29 +0300

    create tests

---
+++ src/test/java/tests/Test01.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Test01 {
+
+    @Test
+    public void test01(){
+
+        Assertions.assertEquals(5,5);
+
+    }
+
+
+}
---
+++ src/test/java/tests/Test02.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Test02  {
+
+    @Test
+    public void test02(){
+
+        Assertions.assertEquals(5,5);
+
+    }
+}
